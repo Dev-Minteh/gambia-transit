ac89bad09b56ff8b7f0b43dd293a1ccf1fe3aa06
Dev-Minteh <latehminteh@gmail.com>
2026-09-16 15:02:52 +0000

    Level 3: display full recommended trip on page

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const button = document.getElementById("searchButton");
 const fromInput = document.getElementById("fromInput");
 const toInput = document.getElementById("toInput");
+const results = document.getElementById("results");
 
 button.addEventListener("click", function(){
 const fromValue = fromInput.value;
 const toValue = toInput.value;
 const allPaths = findAllPaths(routes, fromValue, toValue, []);
-console.log(allPaths);
-})
+const bestTrip = recommendRoute(allPaths, "fare");
+let html = "";
+for(let i = 0; i < best.length; i++){
+    html = html + `<p>First leg: ${bestTrip[i].from} → ${bestTrip[i].to}</p>`;
+}
+results.innerHTML = html;
+});
 
 
