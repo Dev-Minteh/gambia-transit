a28d3583fb2db496afbd136002f3b857b53b9b10
Dev-Minteh <latehminteh@gmail.com>
2026-09-16 15:26:26 +0000

    Level 3: full leg details displayed correctly

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ const allPaths = findAllPaths(routes, fromValue, toValue, []);
 const bestTrip = recommendRoute(allPaths, "fare");
 let html = "";
 for(let i = 0; i < best.length; i++){
-    html = html + `<p>First leg: ${bestTrip[i].from} → ${bestTrip[i].to}</p>`;
+    html += `<p>
+    ${bestTrip[i].from} → ${bestTrip[i].to}  
+    | Vehicle: ${bestTrip[i].vehicle}  
+    | Fare: ${bestTrip[i].fare}  
+    | Time: ${bestTrip[i].travelTime} mins
+  </p>`;
 }
 results.innerHTML = html;
 });
